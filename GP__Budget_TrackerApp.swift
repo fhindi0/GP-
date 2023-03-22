846ba24058b0f63d213b9b74927c608ab6bcfa24
fhindi0 <fayzehindi@gmail.com>
2023-03-21 17:11:41 -0700

    Test

---
+++ GP__Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GP__Budget_TrackerApp.swift
+//  GP$ Budget Tracker
+//
+//  Created by Fayze Hindi on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GP__Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
